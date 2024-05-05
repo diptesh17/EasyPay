73c6e736d9fb1f7b84c58b8a8f6dfdc43f13a91d
diptesh17 <deorediptesh1883@gmail.com>
2024-05-05 13:25:33 +0530

    Added some middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const database = require("./config/database");
 const PORT = process.env.PORT || 4000;
+const cors = require("cors");
 const router = require("./routes/index");
 const app = express();
+
+//  middleware
+app.use(cors());
+app.use(express.json());
 
 // db connect
 database.connect();
 
 // redirect routes
 app.use("/api/v1", router);
-
-// middleware
-app.use(express.json());
 
 // listening port
 app.listen(PORT, () => {
