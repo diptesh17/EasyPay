57244d612a9f9a8ec52c57d2f28700270dc644e8
diptesh17 <deorediptesh1883@gmail.com>
2024-05-05 15:50:27 +0530

    Added bulk and update route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { authMiddleware } = require("../middleware/authmiddleware");
 const router = express.Router();
 
 const zod = require("zod");
@@ -92,4 +93,52 @@ router.post("/signin", async (req, res) => {
   });
 });
 
+const updateBody = zod.object({
+  password: zod.string().optional(),
+  firstName: zod.string().optional(),
+  lastName: zod.string().optional(),
+});
+
+router.put("/", authMiddleware, async (req, res) => {
+  const { success } = updateBody.safeParse(req.body);
+  if (!success) {
+    res.status(411).json({
+      message: "Error while updating information",
+    });
+  }
+
+  await User.updateOne({ _id: req.userId }, req.body);
+
+  res.json({
+    message: "Updated successfully",
+  });
+});
+
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter || "";
+
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          $regex: filter,
+        },
+      },
+      {
+        lastName: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
+
+  res.json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
+});
 module.exports = router;
