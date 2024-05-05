e835d894165943638aec77781e92212fe9f7c42d
diptesh17 <deorediptesh1883@gmail.com>
2024-05-05 13:17:58 +0530

    Initiated routes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const database = require("./config/database");
 const PORT = process.env.PORT || 4000;
+const router = require("./routes/index");
 const app = express();
 
 // db connect
 database.connect();
+
+// redirect routes
+app.use("/api/v1", router);
+
+// middleware
+app.use(express.json());
 
 // listening port
 app.listen(PORT, () => {
