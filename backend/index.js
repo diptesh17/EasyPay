c1df1676a66040aa7ab7d754ea2453ce10f0050f
diptesh17 <deorediptesh1883@gmail.com>
2024-05-05 13:05:15 +0530

    Port and db setup

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
 const express = require("express");
+const database = require("./config/database");
+const PORT = process.env.PORT || 4000;
+const app = express();
 
+// db connect
+database.connect();
 
+// listening port
+app.listen(PORT, () => {
+  console.log(`Server started on : ${PORT}`);
+});
